0812a1d5cddecc98710677447ecae33b18d2b24b
Paulo Zanluqui <paulozanluqui13@gmail.com>
2025-03-12 22:48:28 -0300

    feat: Finalização da primeira versão das rotas de Weather dev: Adição das bibliotecas axios e dotenv

---
+++ src/lib/axios.ts
@@ -0,0 +1,7 @@
+import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const api = axios.create({
+    baseURL: `http://api.weatherapi.com/v1/forecast.json?key=${process.env.API_KEY}`,
+});
--- src/routes/weather.ts
+++ src/routes/weather.ts
@@ -1,8 +1,40 @@
-import { FastifyInstance } from 'fastify'
-import { z } from 'zod'
-import { prisma } from '../lib/prisma'
+import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+import { api } from "../lib/axios";
 
 export async function weatherRoutes(app: FastifyInstance) {
-	//Buscar tempo por cidade
+    app.addHook("onRequest", async (request, reply) => {
+        try {
+            await request.jwtVerify();
+        } catch (error) {
+            reply.status(401).send({ error: "Unauthorized" });
+        }
+    });
+
+    //Buscar tempo por cidade
+    app.get("/weather/:cityName", async (request, reply) => {
+        const getWeatherParamsSchema = z.object({
+            cityName: z.string(),
+        });
+        
+        const { cityName } = getWeatherParamsSchema.parse(request.params);
+
+        const weather = await api.get("", {params: {q: cityName}});
+
+        reply.status(200).send(weather.data)
+    });
+
     //Buscar previsões para 5 dias
+    app.get("/weather/nextdays/:cityName", async (request, reply) => {
+        const getWeatherParamsSchema = z.object({
+            cityName: z.string(),
+        });
+        
+        const { cityName } = getWeatherParamsSchema.parse(request.params);
+
+        const weather = await api.get("", {params: {q: cityName, days: 5}});
+
+        reply.status(200).send(weather.data)
+    });
 }
